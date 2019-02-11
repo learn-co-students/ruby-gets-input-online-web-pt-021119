4319dc878634bd0247cef42b71f2d210a07df516
Henry Sztanski <henry@sztanski.com>
2019-02-11 13:54:11 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,10 @@
 def greeting (name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
+
+# def get_name
+#   puts "Please enter your name:"
+#   name = gets.chomp
+# end
+#
+# greeting (get_name)
