4043c256cf9f2d897bdcb15bc21f88353b0eee20
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-20 03:32:53 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,3 @@
 def greeting(name)
- puts "Hello, {Lena}!"
-end
-def greeting_python
- puts "Hello, Python programmer!"
-end
+ puts "Hi, #{'Lena'}!Welcome to the wonderful world of Ruby programming"
+ end
